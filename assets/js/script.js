5f722b771baa6d0b48e00b0699ded5e04447f204
Harry P <harryepotter@outlook.com>
2023-12-14 00:42:39 +1100

    Added in word-code, thinking out the process and IDs

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+/*Section for Timer*/
+var time = 75;
+
+/* getElementById('time'), subtract time length by 15 on wrong question, end game at 0*/
+
+/* Start button / Quiz Starts / Timer starts count down / Question shown*/
+function startQuiz() {
+
+}
+/* Answered question, next question shown, time either counts down and/or subtracts if wrong answer */
+
+function question() {
+
+}
+
+function answer() {
+
+}
+/*End Quiz if time runs out/answers all questions -> prompt screen*/
+
+function endQuiz() {
+
+}
+/* Save your high score and go to page or go back to main page, already have buttons*/
+
+function saveNewScore() {
+
+}
+
+/*Section for Questions -> need to write questions and answers*/
+var questions = [];
+/*Section for Highscores page, store highscores and clear scores*/
